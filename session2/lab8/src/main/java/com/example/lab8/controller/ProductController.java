4b5e751e6ed7635a07f2abe8c649691c6a1d1ea1
jerome.yang <jerome.yang@gcash.com>
2025-07-19 07:04:22 +0800

    Lab 7

---
+++ session2/lab8/src/main/java/com/example/lab8/controller/ProductController.java
@@ -0,0 +1,24 @@
+package com.example.demo.myfirstapi.controller;
+
+import com.example.demo.myfirstapi.model.Product;
+import com.example.demo.myfirstapi.repository.ProductRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+    private final ProductRepository productRepository; // Declare the dependency
+
+    // Constructor Injection: Spring will automatically provide ProductRepository here
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping("/products")
+    public List<Product> getAllProduct(){
+        return productRepository.findAll();
+    }
+
+}
